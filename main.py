9f4070d8036b46214bdccbea9bd8b32c2c9a786e
MyNameIsTrez <welfje@gmail.com>
2024-01-18 16:18:40 +0100

    Proper metadata

--- main.py
+++ main.py
@@ -9,30 +9,49 @@ cpp_ext = {".c", ".cc", ".cpp", ".c++"}
 cpp_and_hpp_ext = hpp_ext | cpp_ext
 
 
-def recursively_count_occurrences(table, occurrences, include, seen):
+def recursively_count_occurrences(table, meta, counts, include, seen):
     if include in seen:
         return
 
-    occurrences[include] = occurrences.get(include, 0) + 1
+    counts[include] = counts.get(include, 0) + 1
 
     if include in table:
         seen.add(include)
 
+        size = table[include]["size"]
+        meta["total_included_bytes"] += size
+
+        extension = Path(include).suffix
+        if extension in hpp_ext:
+            meta["total_included_hpp_bytes"] += size
+        elif extension in cpp_ext:
+            meta["total_included_cpp_bytes"] += size
+
         for subinclude in table[include]["includes"]:
-            recursively_count_occurrences(table, occurrences, subinclude, seen)
+            recursively_count_occurrences(table, meta, counts, subinclude, seen)
 
 
 def count_occurrences(table):
-    occurrences = {}
+    meta = {
+        "total_included_bytes": 0,
+        "total_included_hpp_bytes": 0,
+        "total_included_cpp_bytes": 0,
+    }
+    counts = {}
+    occurrences = {"meta": meta, "counts": counts}
 
     for name, value in table.items():
         includes = value["includes"]
 
         extension = Path(name).suffix
         if extension in cpp_ext:
+            size = value["size"]
+            meta["total_included_bytes"] += size
+            meta["total_included_cpp_bytes"] += size
+
             for include in includes:
                 seen = set()
-                recursively_count_occurrences(table, occurrences, str(include), seen)
+                recursively_count_occurrences(table, meta, counts, str(include), seen)
 
     return occurrences
 
@@ -90,23 +109,10 @@ def main():
 
     occurrences = count_occurrences(table)
 
-    occurrences["total_inclusions"] = sum(occurrences.values())
+    occurrences["meta"]["total_inclusions"] = sum(occurrences["counts"].values())
 
-    occurrences["total_bytes"] = 0
-    occurrences["total_hpp_bytes"] = 0
-    occurrences["total_cpp_bytes"] = 0
-
-    for name, value in table.items():
-        occurrences["total_bytes"] += value["size"]
-
-        extension = Path(name).suffix
-        if extension in hpp_ext:
-            occurrences["total_hpp_bytes"] += value["size"]
-        elif extension in cpp_ext:
-            occurrences["total_cpp_bytes"] += value["size"]
-
-    occurrences = dict(
-        sorted(occurrences.items(), key=lambda item: item[1], reverse=True)
+    occurrences["counts"] = dict(
+        sorted(occurrences["counts"].items(), key=lambda item: item[1], reverse=True)
     )
     with open(args.output_occurrences_path, "w") as f:
         json.dump(occurrences, f)
